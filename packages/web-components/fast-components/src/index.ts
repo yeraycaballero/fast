d854257238ca3830947bef0f26bd427e5e9f8232
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2020-06-15 12:37:16 -0700

    add export

--- packages/web-components/fast-components/src/index.ts
+++ packages/web-components/fast-components/src/index.ts
@@ -5,6 +5,7 @@ export * from "./breadcrumb/index";
 export * from "./breadcrumb-item/index";
 export * from "./button/index";
 export * from "./card/index";
+export * from "./carousel/index";
 export * from "./checkbox/index";
 export * from "./color/index";
 export * from "./design-system-provider/index";
